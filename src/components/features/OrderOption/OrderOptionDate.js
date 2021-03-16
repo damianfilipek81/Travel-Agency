0688ea503350fddb62a230272fdc6d4d022b00be
Damian Filipek <damianfilipek81@gmail.com>
2021-03-16 19:05:23 +0100

    Add OrderSummary trip days calculator

--- src/components/features/OrderOption/OrderOptionDate.js
+++ src/components/features/OrderOption/OrderOptionDate.js
@@ -1,22 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 import 'react-datepicker/dist/react-datepicker-cssmodules.css';
 import DatePicker from 'react-datepicker';
 import PropTypes from 'prop-types';
 import styles from './OrderOption.scss';
 
-const OrderOptionDate = ({ setOptionValue, currentValue }) => (
-  <div className={styles.component}>
-    <DatePicker
-      className={styles.input}
-      selected={currentValue}
-      onChange={date => setOptionValue(date)}
-    />
-  </div>
-);
+const OrderOptionDate = ({ setOptionValue }) => {
+
+  const [startDate, setStartDate] = useState(new Date());
+  return (
+    <div className={styles.component}>
+      <DatePicker selected={startDate} onChange={date => (setStartDate(date), setOptionValue(date))} />
+    </div>
+  );
+};
 
 OrderOptionDate.propTypes = {
   setOptionValue: PropTypes.func,
-  currentValue: PropTypes.any,
 };
 
 export default OrderOptionDate;
