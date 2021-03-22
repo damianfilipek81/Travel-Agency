66b280460b07526cda9c15437440715e418e8fc1
Damian <damianfilipek81@gmail.com>
2021-03-22 21:11:35 +0100

    Add json-server

--- src/components/features/OrderForm/OrderForm.js
+++ src/components/features/OrderForm/OrderForm.js
@@ -4,9 +4,49 @@ import PropTypes from 'prop-types';
 import OrderSummary from '../OrderSummary/OrderSummary';
 import pricing from '../../../data/pricing.json';
 import OrderOption from '../OrderOption/OrderOption';
+import Button from '../../common/Button/Button';
+import { calculateTotal } from '../../../utils/calculateTotal';
+import { formatPrice } from '../../../utils/formatPrice';
+import settings from '../../../data/settings.js';
+
+const sendOrder = (options, tripCost, id, name, country) => {
+  const totalCost = formatPrice(calculateTotal(tripCost, options));
+
+  const payload = {
+    ...options,
+    totalCost,
+    tripName: name,
+    tripId: id,
+    countryId: country.alpha3Code,
+  };
+  
+  if (options.name == '' || options.contact == '') {
+    alert('Please write you name & contact');
+  } else {
+
+    const url = settings.db.url + '/' + settings.db.endpoint.orders;
+
+    const fetchOptions = {
+      cache: 'no-cache',
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(payload),
+    };
+
+    fetch(url, fetchOptions)
+      .then(function (response) {
+        return response.json();
+      }).then(function (parsedResponse) {
+        console.log('parsedResponse', parsedResponse);
+      });
+  }
+};
 
 const OrderForm = (props) => {
-  const { tripCost, options, setOrderOption, days } = props;
+  const { tripCost, options, setOrderOption, days, id, name, country } = props;
+
   return (
     <Row>
       {pricing.map(option => (
@@ -15,8 +55,9 @@ const OrderForm = (props) => {
         </Col>
       ))}
       <Col xs={12}>
-        <OrderSummary tripCost={tripCost} options={options} days={days}/>
+        <OrderSummary tripCost={tripCost} options={options} days={days} />
       </Col>
+      <Button onClick={() => sendOrder(options, tripCost, id, name, country)}>Order now!</Button>
     </Row>
   );
 };
@@ -26,6 +67,9 @@ OrderForm.propTypes = {
   options: PropTypes.object,
   setOrderOption: PropTypes.func,
   days: PropTypes.number,
+  id: PropTypes.string,
+  name: PropTypes.string,
+  country: PropTypes.object,
 };
 
 export default OrderForm;
--- src/components/views/Trip/TripContainer.js
+++ src/components/views/Trip/TripContainer.js
@@ -1,12 +1,11 @@
-import {connect} from 'react-redux';
+import { connect } from 'react-redux';
 import Trip from './Trip';
-import {getTripById} from '../../../redux/tripsRedux';
-import {getCountryByCode} from '../../../redux/countriesRedux';
+import { getTripById } from '../../../redux/tripsRedux';
+import { getCountryByCode } from '../../../redux/countriesRedux';
 
 const mapStateToProps = (state, props) => {
   const trip = getTripById(state, props.match.params.id);
   const country = getCountryByCode(state, trip.country.code);
-
   return {
     ...trip,
     country,
